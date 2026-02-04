321066790c1b4f466ada6110248914e95dc36621
intern1-ctrl <intern1@markwave.ai>
2026-02-04 15:55:28 +0530

    Added Changes

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -2,5 +2,5 @@ import { Platform } from 'react-native';
 
 export const API_BASE_URL =
     Platform.OS === 'android' && __DEV__
-        ? 'http://10.0.2.2:8000/api'
+        ? 'http://192.168.1.191:8000/api'
         : 'http://34.14.209.166:8000/api';
