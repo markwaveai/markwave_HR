352b927759f8375f035dff1cefa1f697bd39ed94
intern1-ctrl <intern1-ctrl@example.com>
2026-02-05 15:47:31 +0530

    Added Changes

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [react()],
     server: {
       host: true,
       port: 5173,
-      allowedHosts: env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : true
+      allowedHosts: env.VITE_ALLOWED_HOSTS
+        ? env.VITE_ALLOWED_HOSTS.split(',')
+        : 'all'
     }
   }
 })
